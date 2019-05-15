ad6e048efc6bb862b1b4605207e9f9b3479ef30f
Michal Miszczyszyn <michal@mmiszy.pl>
2019-05-15 23:15:16 +0200

    Add GET /:id

--- src/modules/items/ItemsRoute.ts
+++ src/modules/items/ItemsRoute.ts
@@ -1,26 +1,58 @@
-import Joi from "joi";
-import { ServerRegisterPluginObject } from "hapi";
-import { ItemSchema, getAllItems,
-  // getItemById, createItem, updateItem, deleteItem
-} from "src/models/Item";
+import Joi from 'joi';
+import Boom from '@hapi/boom';
+import { ServerRegisterPluginObject } from 'hapi';
+import {
+  ItemSchema,
+  getAllItems,
+  getItemById,
+  createItem,
+  updateItem,
+  deleteItem,
+} from 'src/models/Item';
 
 export const ItemRoutePlugin: ServerRegisterPluginObject<never> = {
   plugin: {
-    name: "Item Routes",
+    name: 'Item Routes',
     async register(server) {
-
       await server.route({
-        method: "GET",
-        path: "/",
+        method: 'GET',
+        path: '/',
         options: {
           response: {
-            schema: Joi.array().items(ItemSchema.optional()).required()
-          }
+            schema: Joi.array()
+              .items(ItemSchema.optional())
+              .required(),
+          },
         },
         async handler() {
           return getAllItems();
-        }
+        },
       });
-    }
-  }
+
+      await server.route({
+        method: 'GET',
+        path: '/:id',
+        options: {
+          response: {
+            schema: ItemSchema.required(),
+          },
+          validate: {
+            params: Joi.object({
+              id: Joi.string().required(),
+            }).required(),
+          },
+        },
+        async handler(request) {
+          const { id } = request.params;
+          
+          const item = await getItemById(id);
+          if (!item) {
+            throw Boom.notFound('Item not found!');
+          }
+
+          return item;
+        },
+      });
+    },
+  },
 };
