bcf9b3369637437bbe7a4f02b5933a7c09058b93
Michal Miszczyszyn <michal@mmiszy.pl>
2019-05-15 22:40:31 +0200

    Add stub CRUD

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,10 +1,26 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-export const ItemSchema = Joi.object({
-  id: Joi.number().required(),
+export const ItemDataSchema = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
-  createdOn: Joi.date().required()
 }).required();
 
+export const ItemSchema = ItemDataSchema.keys({
+  id: Joi.number().required(),
+  createdOn: Joi.date().required(),
+}).required();
+
+export type ItemData = Joi.SchemaValue<typeof ItemDataSchema>;
 export type Item = Joi.SchemaValue<typeof ItemSchema>;
+
+export async function getAllItems(): Promise<Item[]> {
+  return [];
+}
+
+export async function getItemById(id: Item['id']): Promise<Item> {}
+
+export async function createItem(itemData: ItemData): Promise<Item> {}
+
+export async function updateItem(id: Item['id'], itemData: ItemData): Promise<Item> {}
+
+export async function deleteItem(id: Item['id']): Promise<void> {}
