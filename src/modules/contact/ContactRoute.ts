6b62e2f48d904a17461ca03678f6da74a3fac943
Michal Miszczyszyn <michal@mmiszy.pl>
2019-05-15 18:36:28 +0200

    Add contact route

--- src/modules/contact/ContactRoute.ts
+++ src/modules/contact/ContactRoute.ts
@@ -1,10 +1,36 @@
+import Joi from "joi";
 import { ServerRegisterPluginObject } from "hapi";
 
-export const ContactRoutePlugin:ServerRegisterPluginObject<never> = {
+export const ContactRoutePlugin: ServerRegisterPluginObject<never> = {
   plugin: {
-    name: 'Contact Routes',
-    async register(server){
-      // await server.route() // @todo
+    name: "Contact Routes",
+    async register(server) {
+      await server.route({
+        method: "GET",
+        path: "/contact",
+        options: {
+          response: {
+            schema: Joi.object({
+              author: Joi.string().required(),
+              email: Joi.string()
+                .email()
+                .required(),
+              webpage: Joi.string()
+                .uri()
+                .required(),
+              twitterName: Joi.string().required()
+            })
+          }
+        },
+        async handler() {
+          return {
+            author: 'Michał Miszczyszyn',
+            email: 'hi@typeofweb.com',
+            webpage: 'https://typeofweb.com/',
+            twitterName: 'mmiszy',
+          }
+        }
+      });
     }
   }
-}
+};
