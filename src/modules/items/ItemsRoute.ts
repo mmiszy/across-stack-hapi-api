3f0273461e00e5732e9aeab677db3dea163bafb2
Michal Miszczyszyn <michal@mmiszy.pl>
2019-05-16 19:13:10 +0200

    Complete

--- src/modules/items/ItemsRoute.ts
+++ src/modules/items/ItemsRoute.ts
@@ -8,6 +8,7 @@ import {
   createItem,
   updateItem,
   deleteItem,
+  ItemDataSchema,
 } from 'src/models/Item';
 
 export const ItemRoutePlugin: ServerRegisterPluginObject<never> = {
@@ -31,26 +32,84 @@ export const ItemRoutePlugin: ServerRegisterPluginObject<never> = {
 
       await server.route({
         method: 'GET',
-        path: '/:id',
+        path: '/{id}',
         options: {
           response: {
             schema: ItemSchema.required(),
           },
           validate: {
             params: Joi.object({
-              id: Joi.string().required(),
+              id: Joi.number().required(),
             }).required(),
           },
         },
         async handler(request) {
           const { id } = request.params;
-          
+
           const item = await getItemById(id);
           if (!item) {
             throw Boom.notFound('Item not found!');
           }
 
           return item;
+        },
+      });
+
+      await server.route({
+        method: 'POST',
+        path: '/',
+        options: {
+          response: {
+            schema: ItemSchema.required(),
+          },
+          validate: {
+            payload: ItemDataSchema.required(),
+          },
+        },
+        async handler(request) {
+          const item = await createItem(request.payload);
+          return item;
+        },
+      });
+
+      await server.route({
+        method: 'PUT',
+        path: '/{id}',
+        options: {
+          response: {
+            schema: null,
+          },
+          validate: {
+            payload: ItemDataSchema.required(),
+            params: Joi.object({
+              id: Joi.number().required(),
+            }).required(),
+          },
+        },
+        async handler(request) {
+          const { id } = request.params;
+          await updateItem(id, request.payload);
+          return null;
+        },
+      });
+
+      await server.route({
+        method: 'DELETE',
+        path: '/{id}',
+        options: {
+          response: {
+            schema: null,
+          },
+          validate: {
+            params: Joi.object({
+              id: Joi.number().required(),
+            }).required(),
+          },
+        },
+        async handler(request) {
+          const { id } = request.params;
+          await deleteItem(id);
+          return null;
         },
       });
     },
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,46 @@
-import Hapi from "hapi";
-import { getConfig } from "./config";
-import { ContactRoutePlugin } from "./modules/contact/ContactRoute";
-import { ItemRoutePlugin } from "./modules/items/ItemsRoute";
+import Hapi from 'hapi';
+import { getConfig } from './config';
+import { ContactRoutePlugin } from './modules/contact/ContactRoute';
+import { ItemRoutePlugin } from './modules/items/ItemsRoute';
 
 export async function getServer() {
-  const port = getConfig("PORT") || 3001;
+  const port = getConfig('PORT') || 3001;
+
+  const validationOptions = {
+    allowUnknown: false,
+    convert: true,
+    stripUnknown: { objects: true },
+  };
 
   const server = new Hapi.Server({
     port,
     routes: {
-      cors: true
-    }
+      cors: true,
+      response: {
+        modify: true,
+        options: validationOptions,
+      },
+      validate: {
+        options: validationOptions,
+        async failAction(_request, _h, err) {
+          throw err;
+
+          // if (isProd()) … 
+        },
+      },
+    },
   });
 
   server.events.on({ name: 'request', channels: 'error' }, (_request, event, _tags) => {
     console.error(event.error);
   });
 
-  await server.register(ContactRoutePlugin, {
-    routes: {
-      prefix: "/api"
-    }
-  });
+  await server.register(ContactRoutePlugin);
 
   await server.register(ItemRoutePlugin, {
     routes: {
-      prefix: "/api/items"
-    }
+      prefix: '/items',
+    },
   });
 
   return server;
